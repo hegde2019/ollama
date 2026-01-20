26cd317278c05f96c3bc3f8bb6c873c30ca50d98
Patrick Devine <patrick@infrahq.com>
2026-01-20 15:40:57 -0800

    housekeeping: move manifest implementation

--- server/routes.go
+++ server/routes.go
@@ -38,6 +38,7 @@ import (
 	"github.com/ollama/ollama/format"
 	"github.com/ollama/ollama/fs/ggml"
 	"github.com/ollama/ollama/llm"
+	"github.com/ollama/ollama/manifest"
 	"github.com/ollama/ollama/logutil"
 	"github.com/ollama/ollama/manifest"
 	"github.com/ollama/ollama/middleware"
